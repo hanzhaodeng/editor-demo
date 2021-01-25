4e2fcb11c2733ec7e9cf3a13f2424b1ea4c2e001
hanzhaodeng <hanzhaodeng@gmail.com>
2021-01-24 19:58:29 -0800

    Added editor

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Editor msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Editor from "@/components/Editor";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    Editor
   }
 };
 </script>
